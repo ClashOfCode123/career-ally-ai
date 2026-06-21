b50a213a148a1be501e67d30a6271a3e608fdd6d
ArchitaSarkar <architasarkar1602@gmail.com>
2026-06-21 19:55:17 +0530

    fix: correct import order for dns and cors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 
 import dns from "dns";
-dns.setServers(["1.1.1.1", "8.8.8.8"]);import cors from 'cors';
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import http from "http";
